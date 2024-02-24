6241307b2927792705d87a1b6c9fd6992638201d
RMP <ruthiepina@gmail.com>
2024-02-23 20:50:30 -0700

    Added models for Comment and Post, added associations thru index.js in models for them as well. Will add comments for associations at end/refactoring.

--- models/index.js
+++ models/index.js
@@ -11,4 +11,28 @@ Post.belongsTo(User, {
    foreignKey: "user_id",
 });
 
+User.belongsToMany(Post, {
+   foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+   foreignKey: "post_id",
+});
+
+Comment.belongsTo(User, {
+   foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+   foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+   foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+   foreignKey: "post_id",
+});
+
 module.exports = { User, Post, Comment };
